a52019beb86e1d05d0d6bfd061753e1c7b7fd671
Yishi Liu <aprilbaby@Yishis-MacBook-Air.local>
2019-08-17 16:12:16 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import ReactDom from 'react-dom';
+import AppRouter from './router';
+
+const App = () => (
+    <Fragment>
+        <AppRouter />
+    
+    </Fragment>
+);
+
+ReactDom.render(<App />,document.getElementById('root'));
